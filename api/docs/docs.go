ebbd6981d1fec11c24760e4f7c18548bd2f0fa48
Sameh Abouel-saad <samehabouelsaad@gmail.com>
2024-11-03 15:49:50 +0200

    update docs

--- api/docs/docs.go
+++ api/docs/docs.go
@@ -548,7 +548,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.3.0",
+	Version:          "0.2.0",
 	Host:             "",
 	BasePath:         "/",
 	Schemes:          []string{},
